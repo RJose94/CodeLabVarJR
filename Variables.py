78b9ee686e0d167d60b0a7a0eae128dbdbc312d4
RJose94 <137239149+RJose94@users.noreply.github.com>
2023-07-25 18:45:42 -0500

    Variables

---
+++ Variables.py
@@ -0,0 +1,20 @@
+x = 15
+print(type(x)) # tipo entero:representa el valor de los numero enteros
+y = 4.56
+print(type(y)) # tipo (float) (double): representa los numero con valores decimales
+z = 3.123
+print(type("z")) # tipo string: representa toda cadena de texto
+w = True 
+print (type(w)) # tipo booleano: representa 2 valores "true" o "false"
+name="recibos domesticos"
+n=20000
+luz=39000
+agua=25000
+print(agua+luz+n,{name})
+
+
+# en python, los enteros no tienen límite de tamaño.
+# Los números de punto flotante son representados en hardware de computadora como fracciones base 2 (binarias).
+
+
+
